8ec97f8aac9aee15f43b4a36aff7af5a5f74d41a
Nagendra <gadinagendra333@gmail.com>
2023-01-03 08:24:10 +0530

    my first commit to repo

---
+++ src/package123/Class_1.java
@@ -0,0 +1,17 @@
+package package123;
+
+public class Class_1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		System.out.println("selenium project 1");
+		System.out.println("selenium project 1");
+		System.out.println("selenium project 1");
+		System.out.println("selenium project 1");
+
+		
+	}
+
+}
