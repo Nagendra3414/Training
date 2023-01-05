8ae052dbe49c39c34651b9d4c5d519433aee93ce
Prasuna12 <prasunavihaan14@gmail.com>
2023-01-05 15:25:22 +1100

    First pull code

---
+++ src/package123/class2.java
@@ -0,0 +1,11 @@
+package package123;
+
+public class class2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("selenium project 2");
+		System.out.println("selenium project 2");
+	}
+	
+}
