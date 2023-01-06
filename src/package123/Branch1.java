4257f9ccd80b264b0949658db471aca4f4c8cf49
Nagendra <gadinagendra333@gmail.com>
2023-01-06 10:12:46 +0530

    added new classes in the branch C1

---
+++ src/package123/Branch1.java
@@ -0,0 +1,10 @@
+package package123;
+
+public class Branch1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("selenium project _Branch1");
+	}
+
+}
